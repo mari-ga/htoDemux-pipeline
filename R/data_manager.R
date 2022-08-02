921a34fac33e6c0f80ed89c25602bdbd163e4163
mari-ga <mariana.gonzales.andre@gmail.com>
2022-08-02 19:05:53 +0200

    changes to data manager

--- R/data_manager.R
+++ R/data_manager.R
@@ -9,9 +9,19 @@ library(gplots)
 library(argparser, quietly=TRUE)
 
 
-data_dir <- '/home/icb/mariana.gonzales/storage/MS_nuclei_hashing/747495_GX12/outs/filtered_feature_bc_matrix'
-list.files(data_dir) # Should show barcodes.tsv, genes.tsv, and matrix.mtx
-expression_matrix <- Read10X(data.dir = data_dir)
-seurat_object = CreateSeuratObject(counts = expression_matrix)
+data_dir_umi <- '/home/icb/mariana.gonzales/storage/MS_nuclei_hashing/747495_GX12/outs/filtered_feature_bc_matrix'
+list.files(data_dir_umi) # Should show barcodes.tsv, genes.tsv, and matrix.mtx
+umi <- Read10X(data.dir = data_dir_umi)
+seurat_object = CreateSeuratObject(counts = umi)
+saveRDS(seurat_object, file="/home/icb/mariana.gonzales/storage/tranformed_data/GX12_UMI.rds")
+
+
+data_dir_counts <- '/home/icb/mariana.gonzales/storage/MS_nuclei_hashing/combined_GX34/outs/filtered_feature_bc_matrix'
+list.files(data_dir_counts) # Should show barcodes.tsv, genes.tsv, and matrix.mtx
+counts <- Read10X(data.dir = data_dir_counts)
+seurat_object_2 = CreateSeuratObject(counts = counts)
+saveRDS(seurat_object_2, file="/home/icb/mariana.gonzales/storage/tranformed_data/GX12_counts.rds")
+
+
 
 
